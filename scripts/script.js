0ff965f931c30d854639e2f11a6599efd73adc92
mayara-canaver <mayaracanaver@hotmail.com>
2024-12-16 01:29:16 -0300

    creating new function and changing variable name

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,19 @@
-function PlaySound(soundobj) {
-  var thissound = document.getElementById(soundobj);
-  thissound.play();
+function playSound(soundobj) {
+  let audio = document.getElementById(soundobj);
+  
+  audio.play();
 }
 
-function StopSound(soundobj) {
-  var thissound = document.getElementById(soundobj);
-  thissound.pause();
-  thissound.currentTime = 0;
+function stopSound(soundobj) {
+  let audio = document.getElementById(soundobj);
+
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+function turnOn(){
+  const brights = document.querySelectorAll(".bright");
+  brights.forEach(bright => {
+    bright.style.opacity = "0.2";
+  });
 }
